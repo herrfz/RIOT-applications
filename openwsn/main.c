66ffe77663e8baa0859752315f96dec93b7f58a0
Eriza Fazli <erizzaaaaa@gmail.com>
2015-05-09 15:21:26 +0200

    cleaned up openwsn app, with dummyradio

--- openwsn/main.c
+++ openwsn/main.c
@@ -30,28 +30,10 @@
 
 #include "03oos_openwsn.h"
 
-#define MSEC 1000
-#define SEC  (1000 * MSEC)
-
-static char stack_buffer[KERNEL_CONF_STACKSIZE_MAIN];
-
 const shell_command_t shell_commands[] = {
     {"owsn_init", "Start OpenWSN", openwsn_start_thread},
     {NULL, NULL, NULL}
 };
-
-void* blink(void *arg)
-{
-    (void)arg;
-    LED_GREEN_ON;
-    vtimer_usleep(SEC);
-    while(1)
-    {
-        LED_GREEN_TOGGLE;
-        vtimer_usleep(SEC);
-    }
-
-}
 
 static int shell_readc(void)
 {
@@ -71,10 +53,6 @@ int main(void) {
     (void) posix_open(uart0_handler_pid, 0);
 
     puts("Welcome to RIOT!");
-
-    thread_create(stack_buffer, sizeof(stack_buffer),
-                  PRIORITY_MAIN - 2, CREATE_STACKTEST,
-                  blink, NULL, "blink");
 
     shell_init(&shell, shell_commands, UART0_BUFSIZE, shell_readc, shell_putchar);
 
